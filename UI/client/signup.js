0b7d584c3fe695da1326e96341add4503db697e9
Oluseyi Anani <anasey2018@gmail.com>
2018-09-12 11:11:01 +0100

    feat(signupPage):update

--- UI/client/signup.js
+++ UI/client/signup.js
@@ -2,35 +2,36 @@ const btn = document.getElementById('submit');
 
 
 const url = 'https://anasey-stackoverflow-lite.herokuapp.com/api/v1/auth/signup';
-//const url = 'localhost:3000/api/v1/auth/signup';
+// const url = 'localhost:3000/api/v1/auth/signup';
 
 const signupUser = (e) => {
-const fullname = document.getElementById('fullname').value;
-const username = document.getElementById('username').value;
-const gender = document.getElementById('gender').value;
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+  const fullname = document.getElementById('fullname').value;
+  const username = document.getElementById('username').value;
+  const gender = document.getElementById('gender').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-const options = {
-  method: 'POST',
-  body: JSON.stringify({
-    fullname,
-    username,
-    gender,
-    email,
-    password,
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      fullname,
+      username,
+      gender,
+      email,
+      password,
     }),
-  headers: {
-    'Content-Type': 'application/json',
-  }
-};
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
   e.preventDefault();
-  console.log('Options Passed', options)
+  console.log('Options Passed', options);
+
   fetch(url, options)
-  .then((response) => console.log('Response is', response))
-  .then((data)=> console.log('I can append this here', data))
-  .catch((error) => console.error('Error catching URL', error));
-}
+    .then(response => console.log('Response is', response))
+    .then(data => console.log('I can append this here', data))
+    .catch(error => console.error('Error catching URL', error));
+};
 
 btn.addEventListener('click', signupUser);
